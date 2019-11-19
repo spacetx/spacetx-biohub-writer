bcc9deb4a4b1bc96c5eada598be27f058b49ba7d
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2019-11-19 13:17:18 -0800

    update tests

--- tests/test_spacetx_biohub_writer.py
+++ tests/test_spacetx_biohub_writer.py
@@ -12,8 +12,6 @@ def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
     result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'spacetx_biohub_writer.cli.main' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert 'Usage:' in help_result.output
